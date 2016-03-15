852526edfa97b725e20f0f1a0b3f7c7c33b9325c
ja_vazquez <jvazquez@bnl.gov>
2016-03-15 11:10:18 -0400

    added cov_matrix to hadled errorbars

--- DR12/cov_matrix.py
+++ DR12/cov_matrix.py
@@ -7,20 +7,9 @@ file = 'consensus_all_covtot_dM_Hz_fsig.txt'
 file_inv = 'consensus_all_incovtot_dM_Hz_fsig.txt'
 
 m = np.loadtxt(file)
-
-
-#print m
-
-
 n = inv(m)
-#print n
-
-
 np.savetxt(file_inv, n)
 
-
-
-# In[7]:
 
 #Let's blow the error bars wherever we have s8
 data = np.arange(81).reshape((9,9))
@@ -36,15 +25,7 @@ for i in range(9):
 
 
 blown_new = np.array(blown_s8).reshape((9,9))
-#print blown_new
-
-
-
 blown_inv =inv(blown_new)
-#print blown_inv
-
-
-
 np.savetxt('consensus_all_incovtot_dM_Hz_fsig_blown.txt', blown_inv)
 
 
