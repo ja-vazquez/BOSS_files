a2cf37dcfa1cfe509f82653284b2f99c3e10d74f
ja_vazquez <jvazquez@bnl.gov>
2016-03-16 22:23:44 -0400

    incorporated MG into the code

--- DR12/write_files.py
+++ DR12/write_files.py
@@ -14,21 +14,14 @@ def write_ini(model, dataset):
         if 'PLK' in dataset:
             f.write("""
 #Planck 2015, default just include native likelihoods (others require clik)
-DEFAULT(batch2/plik_dx11dr2_HM_v18_TT.ini)
+#DEFAULT(batch2/plik_dx11dr2_HM_v18_TT.ini)
+DEFAULT(batch2/plik_dx11dr2_HM_v18_TTTEEE.ini)
 DEFAULT(batch2/lowTEB.ini)
-DEFAULT(batch2/lensing.ini)
-#DEFAULT(batch2/lowl.ini)  \n\n""")
+#DEFAULT(batch2/lensing.ini)  \n\n""")
 
-        if 'DR12' in dataset:
-            f.write('#DR12 BAO-RSD \n')
-            f.write('DEFAULT(batch2/DR12.ini) \n\n')
-
-        if 'JLA' in dataset:
-            f.write('#Supernovae \n')
-            f.write('DEFAULT(batch2/JLA.ini) \n\n')
-
-        if 'BAO12' in dataset:
-            f.write('DEFAULT(batch2/DR12_blows8.ini) \n\n')
+	if 'BAO12' in dataset: f.write('DEFAULT(batch2/DR12_blows8.ini) \n\n')
+        if 'DR12'  in dataset: f.write('DEFAULT(batch2/DR12.ini) \n\n')
+        if 'JLA'   in dataset: f.write('DEFAULT(batch2/JLA.ini) \n\n')
 
         #write main file
         f.write('DEFAULT(DR12_INI.ini) \n\n')
@@ -40,8 +33,8 @@ DEFAULT(batch2/lensing.ini)
         f.write('param[mnu]    = 0.06 %s \n'%('0 2 0.1 0.1'       if 'mnu' in model else ''))
         f.write('param[nnu]    = 3.046 %s \n'%('2 6 0.1 0.1'      if 'Neff' in model else ''))
         f.write('param[Alens]  = 1 %s \n'%('0 2 0.1 0.1'          if 'Alens' in model else ''))
-
-
+	f.write('param[Afs8]   = 1 %s \n'%('0 2 0.1 0.1'          if 'Afs8' or 'ABfs8' in model else ''))
+	f.write('param[Bfs8]   = 0 %s \n'%('-1 1 0.1 0.1'         if 'ABfs8' in model else ''))
 
 def write_wq(model, dataset):
     name = model+ '_' + dataset
@@ -54,7 +47,7 @@ hostfile: auto
 job_name: %s
 command: |
      source ~/.bashrc;
-     OMP_NUM_THREADS=%%threads%% mpirun -hostfile %%hostfile%% ./cosmomc INI_%s.ini > chains/logs/INI_%s.log 2>chains/logs/INI_%s.err
+     OMP_NUM_THREADS=%%threads%% mpirun -hostfile %%hostfile%% ./cosmomc INI_%s.ini > chains_new/logs/INI_%s.log 2>chains_new/logs/INI_%s.err
     """%(name, name, name, name)
 
     with open('wq_' + name + '.ini', 'w') as f:
@@ -73,16 +66,11 @@ def write_dist(model, dataset):
 
         f.write('plot%i = omegam H0 \n'%(i));        i+=1
 
-        if 'Ok' in model:
-            f.write('plot%i = omegak H0 \n'%(i));    i+=1
-        if 'w' in model:
-            f.write('plot%i = w H0 \n'%(i));         i+=1
-        if 'mnu' in model:
-            f.write('plot%i = mnu H0 \n'%(i));       i+=1
-        if 'Neff' in model:
-            f.write('plot%i = nnu H0 \n'%(i));       i+=1
-        if 'Alens' in model:
-            f.write('plot%i = Alens H0 \n'%(i));     i+=1
+        if 'Ok' in model:    f.write('plot%i = omegak H0 \n'%(i));    i+=1
+        if 'w' in model:     f.write('plot%i = w H0 \n'%(i));         i+=1
+        if 'mnu' in model:   f.write('plot%i = mnu H0 \n'%(i));       i+=1
+        if 'Neff' in model:  f.write('plot%i = nnu H0 \n'%(i));       i+=1
+        if 'Alens' in model: f.write('plot%i = Alens H0 \n'%(i));     i+=1
 
         f.write('plot_2D_num = %i \n\n'%(i-1))
 
@@ -101,7 +89,7 @@ def write_dist(model, dataset):
 def print_info():
      print 'Usage:'
      print '---'
-     print 'python write_files run/dist all/Alens/[model,dataset]'
+     print 'python write_files run/dist all/neutrino/fs8[models, datasets]'
      print '---'
 
 
@@ -116,16 +104,19 @@ else:
      to_do = '%s'%(sys.argv[1])
 
 if len(sys.argv) > 2:
-   if len(sys.argv) > 3:
-       modell = sys.argv[2].split(',')
-       datasetl = sys.argv[3].split(',')
 
    if sys.argv[2] == 'all':
         modell = ['LCDM','wCDM','OkwCDM','mnu','Neff']
         datasetl = ['PLK+DR12+JLA', 'PLK+DR12', 'PLK']
-   elif sys.argv[2] == 'Alens':
-        modell = ['Alens_LCDM','Alens_wCDM','Alens_OkwCDM','Alens_mnu', 'Alens_Neff']
-        datasetl = ['PLK+BAO12']
+   elif sys.argv[2] == 'neutrino':
+        modell = ['mnu','Neff','Alens_mnu','Alens_Neff']
+        datasetl = ['PLK+BAO12', 'PLK+DR12']
+   elif sys.argv[2] == 'fs8':
+	modell = ['Afs8_LCDM','ABfs8_LCDM']
+	datasetl = ['PLK+DR12']
+   elif len(sys.argv) > 3:
+        modell = sys.argv[2].split(',')
+        datasetl = sys.argv[3].split(',')
 
 try:
  for model in modell:
@@ -134,7 +125,7 @@ try:
         if to_do == 'run':
            write_ini(model, dataset)
            write_wq(model, dataset)
-#           os.system('nohup wq sub  wq_%s_%s.ini &'%(model,dataset))
+           os.system('nohup wq sub  wq_%s_%s.ini &'%(model,dataset))
 
         if to_do == 'dist':
            write_dist(model, dataset)
